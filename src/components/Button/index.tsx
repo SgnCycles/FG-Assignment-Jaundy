6d57edf59637d158e9183dc43cb4090c25d3a306
SgnCycles <signe.bekere@gmail.com>
2026-05-10 11:34:03 +0200

    fix: code tweeks

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,9 +3,9 @@ import type { ButtonType } from '../../types/types'
 const Button = ({click, icon}:ButtonType) => {
   
   return (
-    <div className='button relative flex items-center justify-center cursor-pointer rounded-2xl bg-secondary h-(--button-size-mobile) w-(--button-size-mobile) md:h-(--button-size) md:w-(--button-size) shadow-(--button-shadow) before:content-none before:rounded-full before:absolute before:top-1/2 before:left-1/2 before:h-full before:w-full before:ml-(--button-before-margin-mobile) before:mt-(--button-before-margin-mobile) md:before:ml-(--button-before-margin) md:before:mt-(--button-before-margin) before:bg-button-secondary before:opacity-[.2]' onClick={click}>
-      <span className='circle absolute block rounded-full bg-button-primary h-(--circle-size-mobile) w-(--circle-size-mobile) md:h-(--circle-size) md:w-(--circle-size) ml-(--circle-margin-mobile) mt-(--circle-margin-mobile) md:ml-(--circle-margin) md:mt-(--circle-margin) shadow-(--circle-shadow-mobile) md:shadow-(--circle-shadow) top-1/2 left-1/2'></span>
-      <div className='label flex items-center justify-center absolute h-full w-full opacity-[.8]'><img className='h-6 md:h-8 w-auto' src={icon} height={10}></img>
+    <div className='button border border-solid border-(--button-border-colour) relative flex items-center justify-center cursor-pointer rounded-2xl bg-secondary h-(--button-size-mobile) w-(--button-size-mobile) md:h-(--button-size) md:w-(--button-size) shadow-(--button-shadow) before:content-none before:rounded-full before:absolute before:top-1/2 before:left-1/2 before:h-full before:w-full before:ml-(--button-before-margin-mobile) before:mt-(--button-before-margin-mobile) md:before:ml-(--button-before-margin) md:before:mt-(--button-before-margin) before:bg-button-secondary before:opacity-[.2] before:shadow-(--button-before-shadow:)' onClick={click}>
+      <span className='circle absolute block rounded-full bg-button-primary h-(--circle-size-mobile) w-(--circle-size-mobile) md:h-(--circle-size) md:w-(--circle-size) ml-(--circle-margin-mobile) mt-(--circle-margin-mobile) md:ml-(--circle-margin) md:mt-(--circle-margin) shadow-(--circle-shadow-mobile) md:shadow-(--circle-shadow) top-1/2 left-1/2 blur-[1px] transition-all duration-300 ease-[cubic-bezier(0.23,1,0.32,1)]'></span>
+      <div className='label flex items-center justify-center absolute h-full w-full opacity-[.8] transition-colors duration-300 ease-out'><img className='h-6 md:h-8 w-auto' src={icon} height={10}></img>
       </div>
     </div>
   )
